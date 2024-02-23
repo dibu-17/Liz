86824dd63e339eae3aed3b8df2551c2b0f3ca751
JuanDiego <79742142+dibu-17@users.noreply.github.com>
2024-02-23 02:13:30 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
+
+function changeTextColor() {
+    document.querySelector('.center-text').style.color = getRandomColor();
+}
+
+setInterval(changeBackgroundColor, 1000); 
+setInterval(changeTextColor, 100);
